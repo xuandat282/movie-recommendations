8e87cf89e1242295354953ea21b588546de4ac96
Xuan Dat <xuandat282@gmail.com>
2023-05-04 13:53:13 +0700

    a

--- Neo4j-UI/src/app/screens/home/home.component.ts
+++ Neo4j-UI/src/app/screens/home/home.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {HttpClient, HttpHeaders} from "@angular/common/http";
+import {Observable} from "rxjs";
+import {MoviesService} from "../../services/movies.service";
+import {environment} from "../../../environments/environment";
+import {Movie} from "../../models/movie";
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
-
+export class HomeComponent implements OnInit{
+  movies?: Movie[];
+  constructor(private productService: MoviesService, private http: HttpClient) {}
+  ngOnInit(): void {
+    this.getProducts();
+  }
+  getProducts(): void {
+    this.productService.getProducts().subscribe(product => {
+      this.movies = product;
+    });
+  }
 }
---
+++ Neo4j-UI/src/app/services/movies.service.ts
@@ -0,0 +1,15 @@
+import {Injectable} from '@angular/core';
+import {Observable} from "rxjs";
+import {HttpClient} from "@angular/common/http";
+import {environment} from "../../environments/environment";
+import {Movie} from "../models/movie";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MoviesService {
+  constructor(private http: HttpClient) { }
+  getProducts(): Observable<Movie[]> {
+    return this.http.get<Movie[]>(`${environment.apiUrl}/api/top/movie/top-n/10`);
+  }
+}
